fa7b8b0b3e160e3d88df566637bd38acf603b32d
Wilson Coelho <wlfcoelho@gmail.com>
2025-11-04 23:30:34 -0300

    refactor OrderPersistenceEntity repository naming; add integration tests for order persistence

--- microservices/ordering/src/main/java/com/algaworks/algashop/ordering/infrastructure/persistence/repository/OrderPersistenceEntityRepository.java
+++ microservices/ordering/src/main/java/com/algaworks/algashop/ordering/infrastructure/persistence/repository/OrderPersistenceEntityRepository.java
@@ -3,5 +3,5 @@ package com.algaworks.algashop.ordering.infrastructure.persistence.repository;
 import com.algaworks.algashop.ordering.infrastructure.persistence.entity.OrderPersistenceEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderPersistenceEntityrepository extends JpaRepository<OrderPersistenceEntity, Long> {
+public interface OrderPersistenceEntityRepository extends JpaRepository<OrderPersistenceEntity, Long> {
 }
