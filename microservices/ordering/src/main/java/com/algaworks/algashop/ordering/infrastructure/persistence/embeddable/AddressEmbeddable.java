86d9ae73a99b3f56dbc37a161f5158069d32c159
Wilson Coelho <wlfcoelho@gmail.com>
2026-01-14 23:11:39 -0300

    add embeddable classes for billing, shipping, address, and recipient; update OrderPersistenceEntity to include new fields

---
+++ microservices/ordering/src/main/java/com/algaworks/algashop/ordering/infrastructure/persistence/embeddable/AddressEmbeddable.java
@@ -0,0 +1,23 @@
+package com.algaworks.algashop.ordering.infrastructure.persistence.embeddable;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+public class AddressEmbeddable {
+
+  private String street;
+  private String complement;
+  private String neighborhood;
+  private String number;
+  private String city;
+  private String state;
+  private String zipCode;
+}
---
+++ microservices/ordering/src/main/java/com/algaworks/algashop/ordering/infrastructure/persistence/embeddable/BillingEmeddable.java
@@ -0,0 +1,24 @@
+package com.algaworks.algashop.ordering.infrastructure.persistence.embeddable;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Embedded;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+public class BillingEmeddable {
+
+  private String firstName;
+  private String lastName;
+  private String document;
+  private String phone;
+
+  @Embedded
+  private AddressEmbeddable address;
+}
